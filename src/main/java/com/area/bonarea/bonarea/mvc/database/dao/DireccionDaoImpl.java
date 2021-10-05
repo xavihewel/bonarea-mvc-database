5c1aad891bccaf523c9b80b451997fe3443d70a1
BONAREA\xavier.verges <xavier.verges@bonarea.com>
2021-10-05 12:44:45 +0200

    treballant en la capa DAO

--- src/main/java/com/area/bonarea/bonarea/mvc/database/dao/DireccionDaoImpl.java
+++ src/main/java/com/area/bonarea/bonarea/mvc/database/dao/DireccionDaoImpl.java
@@ -21,8 +21,7 @@ public class DireccionDaoImpl extends BaseDao<Direccion> implements DireccionDao
     private static final String TABLE = "direccion_verges";
     private static final String SQL_GETBYID = "SELECT * FROM " + TABLE
             + "WHERE ID=?";
-    private static final String SQL_GETALL = "SELECT * FROM " + TABLE;
-    private static final String SQL_ADD = "INSERT INTO " + TABLE
+        private static final String SQL_ADD = "INSERT INTO " + TABLE
             + "(calle, poblacion, provincia) VALUES(?,?,?)";
     
     @Override
@@ -35,17 +34,9 @@ public class DireccionDaoImpl extends BaseDao<Direccion> implements DireccionDao
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public List<Direccion> getAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     @Override
     public Direccion add(Direccion direccion) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    
-
-
 }
