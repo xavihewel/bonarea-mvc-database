5c1eae5099b0985cfdc2b41ebc357c88d1b207e5
BONAREA\xavier.verges <xavier.verges@bonarea.com>
2021-10-07 10:43:10 +0200

    usar transaccions

--- src/main/java/com/area/bonarea/bonarea/mvc/database/dao/PersonaDaoImpl.java
+++ src/main/java/com/area/bonarea/bonarea/mvc/database/dao/PersonaDaoImpl.java
@@ -24,7 +24,7 @@ public class PersonaDaoImpl extends BaseDao<Persona, Long> implements PersonaDao
     private static final String KEY_NAME = "id";
     private static final String INSERT_STATEMENT = "INSERT INTO " + TABLE
             + "(nombre, apellidos, dni, FK_direccion_persona) VALUES(?,?,?,?)";
-    
+
     @Inject
     private DireccionDao direccionDao;
 
@@ -42,23 +42,37 @@ public class PersonaDaoImpl extends BaseDao<Persona, Long> implements PersonaDao
     @Override
     public Persona add(Persona persona) throws SQLException {
         long generatedKey = 0;
+        try {
+        } catch (Exception ex) {
+
+        }
         try ( Connection con = this.getConnection();  PreparedStatement preparedInsert = con.prepareStatement(INSERT_STATEMENT,
                 PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-            preparedInsert.setString(1, persona.getNombre());
-            preparedInsert.setString(2, persona.getApellidos());
-            preparedInsert.setString(3, persona.getDni());
-            
-            preparedInsert.setLong(4, this.direccionDao.add(persona.getDireccion()).getId());
-                
-            preparedInsert.executeUpdate();
-            try ( ResultSet resultset = preparedInsert.getGeneratedKeys()) {
-                while (resultset.next()) {
-                    generatedKey = resultset.getLong(1);
-                    persona.setId(generatedKey);
-                    System.out.println("Clave generada = " + generatedKey);
+            try {
+                con.setAutoCommit(false);
+                preparedInsert.setString(1, persona.getNombre());
+                preparedInsert.setString(2, persona.getApellidos());
+                preparedInsert.setString(3, persona.getDni());
+
+                preparedInsert.setLong(4, this.direccionDao.add(persona.getDireccion()).getId());
+
+                preparedInsert.executeUpdate();
+
+                con.commit();
+
+                try ( ResultSet resultset = preparedInsert.getGeneratedKeys()) {
+                    while (resultset.next()) {
+                        generatedKey = resultset.getLong(1);
+                        persona.setId(generatedKey);
+                        System.out.println("Clave generada = " + generatedKey);
+                    }
                 }
+            } catch (Exception ex) {
+                con.rollback();
+                throw ex;
             }
+
         }
         return this.getById(generatedKey);
     }
